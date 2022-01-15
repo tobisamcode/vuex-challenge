cf784ebc057238ea619436aa11e025cd3e867a7c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-01-15 11:29:35 +0100

    vuex challenge

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex';
+
+import productsModule from './modules/product';
+import cartModule from './modules/cart';
+
+const store = createStore({
+    modules: {
+        prods: productsModule,
+        cart: cartModule,
+    }
+
+});
+
+export default store;
