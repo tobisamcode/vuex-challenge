b0a2613548459ad31ea6f63b03bf765821dc6757
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-01-17 15:19:11 +0100

    vuex challenge

--- src/pages/UserCart.vue
+++ src/pages/UserCart.vue
@@ -4,7 +4,7 @@
     <h3>Total Amount: <base-badge mode="elegant">${{ cartTotal }}</base-badge></h3>
     <ul>
       <cart-item
-        v-for="item in cart.items"
+        v-for="item in cartItems"
         :key="item.productId"
         :prod-id="item.productId"
         :title="item.title"
--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,4 +1,5 @@
 export default {
+    namespaced: true,
     state() {
         return {
             cart: { items: [], total: 0, qty: 0 },
