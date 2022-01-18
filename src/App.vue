ea6ab6f6423835f99711bc43bb31797d5a30c8f9
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-01-18 23:49:05 +0100

    vuex challenge

--- src/App.vue
+++ src/App.vue
@@ -9,30 +9,7 @@ import TheHeader from './components/nav/TheHeader.vue';
 export default {
   components: {
     TheHeader
-  },
-  data() {
-    return {
-      isLoggedIn: false,
-    };
-  },
-  provide() {
-    return {
-      isLoggedIn: this.isLoggedIn,
-      cart: this.cart,
-      addProductToCart: this.addProductToCart,
-      removeProductFromCart: this.removeProductFromCart,
-      login: this.login,
-      logout: this.logout,
-    };
-  },
-  methods: {
-    login() {
-      this.isLoggedIn = true;
-    },
-    logout() {
-      this.isLoggedIn = false;
-    },
-  },
+  }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,33 @@ const store = createStore({
     modules: {
         prods: productsModule,
         cart: cartModule,
+    },
+    state() {
+        return {
+            isLoggedIn: False,
+        };
+    },
+    mutations: {
+        login(state) {
+            state.isLoggedIn = true;
+        },
+        logout(state) {
+            state.isLoggedIn = false;
+        },
+    },
+    actions: {
+        login(context) {
+            context.commit('login');
+        },
+        logout(context) {
+            context.commit('logout');
+        },
+    },
+    getters: {
+        isAuthenticated(state) {
+            return state.isLoggedIn;
+        }
     }
-
 });
 
 export default store;
